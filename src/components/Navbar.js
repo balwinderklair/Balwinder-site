a62b073173c1384bb63f165c4650fc6f918d62a6
balwinder klair <46516349+balwinderklair@users.noreply.github.com>
2019-10-26 11:52:44 +0530

    new site

---
+++ src/components/Navbar.js
@@ -0,0 +1,85 @@
+import React, { useState } from "react";
+import NavbarContainer from "./NavbarContainer";
+import { Link } from "./../util/router.js";
+import { useAuth } from "./../util/auth.js";
+import "./Navbar.scss";
+
+function Navbar(props) {
+  const auth = useAuth();
+  const [menuOpen, setMenuOpen] = useState(false);
+
+  return (
+    <NavbarContainer spaced={props.spaced} color={props.color}>
+      <div className="container">
+        <div className="navbar-brand">
+          <div className="navbar-item">
+            <Link to="/">
+              <img className="image" src={props.logo} alt="Logo" />
+            </Link>
+          </div>
+          <div
+            className={"navbar-burger burger" + (menuOpen ? " is-active" : "")}
+            onClick={() => setMenuOpen(!menuOpen)}
+          >
+            <span />
+            <span />
+            <span />
+          </div>
+        </div>
+        <div className={"navbar-menu" + (menuOpen ? " is-active" : "")}>
+          <div className="navbar-start">
+            {auth.user && (
+              <div className="navbar-item has-dropdown is-hoverable">
+                <Link className="navbar-link" to="/">
+                  Account
+                </Link>
+                <div className="navbar-dropdown is-boxed">
+                  <Link className="navbar-item" to="/dashboard">
+                    Dashboard
+                  </Link>
+                  <Link
+                    className="navbar-item"
+                    to="/signout"
+                    onClick={e => {
+                      e.preventDefault();
+                      auth.signout();
+                    }}
+                  >
+                    Sign out
+                  </Link>
+                </div>
+              </div>
+            )}
+
+            {!auth.user && (
+              <>
+                <Link className="navbar-item" to="/signin">
+                  Home
+                </Link>
+                <Link className="navbar-item" to="/signin">
+                  About
+                </Link>
+                <Link className="navbar-item" to="/signin">
+                  Support
+                </Link>
+              </>
+            )}
+          </div>
+          <div className="navbar-end">
+            <Link
+              className="NavbarComponent__become-a-rider navbar-item"
+              to="/"
+            >
+              Become a Rider
+            </Link>
+            <Link className="navbar-item" to="/">
+              Link
+            </Link>
+          </div>
+        </div>
+      </div>
+    </NavbarContainer>
+  );
+}
+
+export default Navbar;
